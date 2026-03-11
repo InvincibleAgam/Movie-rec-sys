02f48cc1edb9ac487844b493c224955c3344ff38
Agam Manash Roy <agam.roy@iiitb.ac.in>
2026-03-11 09:22:57 +0530

    Add Movie entity with MongoDB mapping

---
+++ src/main/java/com/MovieRecSys/Movie/Movie.java
@@ -0,0 +1,29 @@
+package com.MovieRecSys.Movie;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@Document(collection="movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    //The database will only store the ids of the reviews and the reviews themselves will be in separate collection
+    //Also known as manual reference relationship over here
+    @DocumentReference
+    private List<Review> reviews;
+}
